4040ee4b194d81380390765bd74f062487574858
anamul-hoque-shourov <hoqueanamul1998@gmail.com>
2025-03-02 19:06:46 +0600

    funcionalized the task completed and remaining section

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,25 @@
+// complete button functionality
+
+let taskRemaining = document.querySelectorAll(".complete-btn").length;
+document.getElementById("task-remaining").innerText = taskRemaining;
+
+let taskCompleted = 0;
+document.getElementById("task-completed").innerText = taskCompleted;
+
+document.querySelectorAll(".complete-btn").forEach((completeButton) => {
+	completeButton.addEventListener("click", function () {
+		if (!this.disabled) {
+			this.disabled = true;
+
+			taskRemaining--;
+			taskCompleted++;
+
+			document.getElementById("task-remaining").innerText = taskRemaining;
+			document.getElementById("task-completed").innerText = taskCompleted;
+		}
+	});
+});
+
 // background color change
 
 const colorTray = ["beige", "coral", "bisque", "lavender", "pink", "plum", ""];
@@ -14,7 +36,7 @@ document
 function updateDate() {
 	const currentDate = new Date();
 	const displayDatetime = {
-		weekday: "long",
+		weekday: "short",
 		month: "long",
 		day: "numeric",
 		year: "numeric",
@@ -28,3 +50,7 @@ setInterval(updateDate, 1000);
 updateDate();
 
 // activity log update
+
+document.getElementById("btn-history").addEventListener("click", function () {
+	document.getElementById("history").innerHTML = "";
+});
